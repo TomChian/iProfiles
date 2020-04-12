22ccbcc6ed83eecda18a30730b354f2f309730e7
iKangnir Cribug <iKangnir@users.noreply.github.com>
2020-04-12 18:24:42 +0800

    MIMO

---
+++ Surge/Scripts/Unlock/mimo.js
@@ -0,0 +1,40 @@
+/*
+http-response ^https:\/\/api\.revenuecat\.com\/.*\/(receipts$|subscribers) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/langkhach270389/Scripting/master/mojo.vip.js,script-update-interval=0
+http-response ^https:\/\/api\.getmimo\.com\/v1\/subscriptions$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/langkhach270389/Scripting/master/mimo.vip.js,script-update-interval=0
+api.revenuecat.com
+api.getmimo.com
+*/
+var obj = JSON.parse($response.body);
+
+obj= {
+  "source": "ios",
+  "status": "active",
+  "interval": "yearly",
+  "billingInfo": {
+    "currency": "USD",
+    "nextBillingPrice": 59.98999999999999
+  },
+  "subscriptions": [
+    {
+      "source": "ios",
+      "status": "active",
+      "interval": "yearly",
+      "billingInfo": {
+        "currency": "USD",
+        "nextBillingPrice": 59.98999999999999
+      },
+      "intervalCount": 1,
+      "activeUntil": "2099-10-10T08:04:21+00:00",
+      "createdAt": "2019-10-03T08:04:21+00:00",
+      "clientSecret": "",
+      "isActive": true
+    }
+  ],
+  "intervalCount": 1,
+  "activeUntil": "2099-10-10T08:04:21+00:00",
+  "createdAt": "2019-10-03T08:04:21+00:00",
+  "clientSecret": "",
+  "isActive": true
+};
+
+$done({body: JSON.stringify(obj)});
